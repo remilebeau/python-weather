e0818739544b4ebf0f123507739a5551658fc7f1
RemiLeBeau <remilebeau90@gmail.com>
2023-10-16 15:35:58 -0400

    adjusted temperature display to nearest whole degree instead of nearest tenth degree

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ def get_weather():
     if not weather_data["cod"] == 200:
         return render_template("city-not-found.html")
 
-    return render_template("weather.html", title=weather_data["name"], status=weather_data["weather"][0]["description"].capitalize(), temp=f"{weather_data['main']['temp']:.1f}", feels_like=f"{weather_data['main']['feels_like']:.1f}")
+    return render_template("weather.html", title=weather_data["name"], status=weather_data["weather"][0]["description"].capitalize(), temp=f"{weather_data['main']['temp']:.0f}", feels_like=f"{weather_data['main']['feels_like']:.0f}")
 
 
 if __name__ == "__main__":
